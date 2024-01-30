ab4a3675049ecf6c42df1f33ea43c9bc5887408d
konstantin <prospero19902506@gmail.com>
2024-01-30 22:33:38 +0300

    projects and blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(
+        'Текст поста',
+        help_text='Введите текст поста'
+    )
+    pub_date = models.DateTimeField(
+        u'Дата публикации',
+        auto_now_add=True
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='projects/images/',
+        blank=True
+    )
+    url = models.URLField(blank=True)
+    # Аргумент upload_to указывает директорию,
+    # в которую будут загружаться пользовательские файлы.
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.text[:15]
+
