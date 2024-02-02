3ab88689fcea0f04dd43f0941e98e45dbaa55025
konstantin <prospero19902506@gmail.com>
2024-02-02 19:31:41 +0300

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portfolio_db',
-        'USER': 'prospero2',
-        'PASSWORD': '2363669',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
